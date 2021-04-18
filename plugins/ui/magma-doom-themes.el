905291b160185697f1f96f6249f0c977b2075764
Ultra-Hax <68920205+Ultra-Hax@users.noreply.github.com>
2021-04-18 15:34:39 -0400

    .

--- plugins/ui/magma-doom-themes.el
+++ plugins/ui/magma-doom-themes.el
@@ -6,13 +6,13 @@
              doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
        ;; Enable flashing mode-line on errors
-      ;; (doom-themes-visual-bell-config)
+       (doom-themes-visual-bell-config)
 	   
        ;; Enable custom neotree theme (all-the-icons must be installed!)
        (doom-themes-neotree-config)
        ;; or for treemacs users
        (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-		   (doom-themes-treemacs-config)
+       (doom-themes-treemacs-config)
 			     
        ;; Corrects (and improves) org-mode's native fontification.
        (doom-themes-org-config))
